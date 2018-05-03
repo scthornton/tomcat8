d5f02dd5c0a57d637069567a68253bff87f6a753
scthornton <scthornton@gmail.com>
2018-05-03 15:29:06 -0400

    attributes

---
+++ default.rb
@@ -0,0 +1,10 @@
+
+default['tomcat8']['download_url'] = "http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz"
+default['tomcat8']['install_location'] = '/var/tomcat8'
+default['tomcat8']['port'] = 8080
+default['tomcat8']['ssl_port'] = 8443
+default['tomcat8']['ajp_port'] = 8009
+default['tomcat8']['java_options'] = "-Xmx128M"
+default['tomcat8']['tomcat_user'] = "root"
+default['tomcat8']['autostart'] = "true"
+
